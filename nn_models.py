d56bbd19056ecf097aee0cd309ef0aa38b9a4110
nirnayroy <32942494+nirnayroy@users.noreply.github.com>
2021-03-10 02:14:32 +0530

    Update nn_models.py

--- nn_models.py
+++ nn_models.py
@@ -7,11 +7,11 @@ from utils import choose_nonlinearity
 
 class MLP(torch.nn.Module):
   '''Just a salt-of-the-earth MLP'''
-  def __init__(self, input_dim, hidden_dim, output_dim, nonlinearity='tanh'):
+  def __init__(self, input_dim, hidden_dim, output_dim, nonlinearity='tanh', device='cpu'):
     super(MLP, self).__init__()
-    self.linear1 = torch.nn.Linear(input_dim, hidden_dim)
-    self.linear2 = torch.nn.Linear(hidden_dim, hidden_dim)
-    self.linear3 = torch.nn.Linear(hidden_dim, output_dim, bias=None)
+    self.linear1 = torch.nn.Linear(input_dim, hidden_dim).to(device)
+    self.linear2 = torch.nn.Linear(hidden_dim, hidden_dim).to(device)
+    self.linear3 = torch.nn.Linear(hidden_dim, output_dim, bias=None).to(device)
 
     for l in [self.linear1, self.linear2, self.linear3]:
       torch.nn.init.orthogonal_(l.weight) # use a principled initialization
