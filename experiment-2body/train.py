3ba27e3d898517418ca08234efbc40b9bd7e3f4c
nirnayroy <32942494+nirnayroy@users.noreply.github.com>
2021-03-10 02:15:20 +0530

    Update train.py

--- experiment-2body/train.py
+++ experiment-2body/train.py
@@ -44,7 +44,7 @@ def train(args):
     print("Training baseline model:" if args.baseline else "Training HNN model:")
 
   output_dim = args.input_dim if args.baseline else 2
-  nn_model = MLP(args.input_dim, args.hidden_dim, output_dim, args.nonlinearity)
+  nn_model = MLP(args.input_dim, args.hidden_dim, output_dim, args.nonlinearity, device=device)
   model = HNN(args.input_dim, differentiable_model=nn_model.to(device),
             field_type=args.field_type, baseline=args.baseline, device=device)
 
