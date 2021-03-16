b03f2ba44d92ba326f7e4f63b3da8fffba929706
nirnayroy <32942494+nirnayroy@users.noreply.github.com>
2021-03-16 14:12:21 +0530

    Update train.py

--- experiment-2body/train.py
+++ experiment-2body/train.py
@@ -9,29 +9,30 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(PARENT_DIR)
 
-from nn_models import MLP
+from nn_models import MLPAutoencoder
 from hnn import HNN
 from data import get_dataset
 from utils import L2_loss, to_pickle, from_pickle, give_min_and_dist, scale, unscale
 
 def get_args():
-    parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('--input_dim', default=4, type=int, help='dimensionality of input tensor')
-    parser.add_argument('--hidden_dim', default=512, type=int, help='hidden dimension of mlp')
-    parser.add_argument('--learn_rate', default=1e-5, type=float, help='learning rate')
-    parser.add_argument('--batch_size', default=200, type=int, help='batch_size')
-    parser.add_argument('--input_noise', default=0.0, type=int, help='std of noise added to inputs')
-    parser.add_argument('--nonlinearity', default='tanh', type=str, help='neural net nonlinearity')
-    parser.add_argument('--total_steps', default=1000, type=int, help='number of gradient steps')
-    parser.add_argument('--print_every', default=200, type=int, help='number of gradient steps between prints')
-    parser.add_argument('--name', default='2body', type=str, help='only one option right now')
-    parser.add_argument('--baseline', dest='baseline', action='store_true', help='run baseline or experiment?')
-    parser.add_argument('--verbose', dest='verbose', action='store_true', help='verbose?')
-    parser.add_argument('--field_type', default='solenoidal', type=str, help='type of vector field to learn')
-    parser.add_argument('--seed', default=0, type=int, help='random seed')
-    parser.add_argument('--save_dir', default=THIS_DIR, type=str, help='where to save the trained model')
-    parser.set_defaults(feature=True)
-    return parser.parse_args()
+  parser = argparse.ArgumentParser(description=None)
+  parser.add_argument('--input_dim', default=4, type=int, help='dimensionality of input tensor')
+  parser.add_argument('--hidden_dim', default=200, type=int, help='hidden dimension of mlp')
+  parser.add_argument('--latent_dim', default=4, type=int, help='latent dimension of mlp')
+  parser.add_argument('--learn_rate', default=1e-5, type=float, help='learning rate')
+  parser.add_argument('--batch_size', default=200, type=int, help='batch_size')
+  parser.add_argument('--input_noise', default=0.0, type=int, help='std of noise added to inputs')
+  parser.add_argument('--nonlinearity', default='tanh', type=str, help='neural net nonlinearity')
+  parser.add_argument('--total_steps', default=1000, type=int, help='number of gradient steps')
+  parser.add_argument('--print_every', default=200, type=int, help='number of gradient steps between prints')
+  parser.add_argument('--name', default='2body', type=str, help='only one option right now')
+  parser.add_argument('--baseline', dest='baseline', action='store_true', help='run baseline or experiment?')
+  parser.add_argument('--verbose', dest='verbose', action='store_true', help='verbose?')
+  parser.add_argument('--field_type', default='solenoidal', type=str, help='type of vector field to learn')
+  parser.add_argument('--seed', default=0, type=int, help='random seed')
+  parser.add_argument('--save_dir', default=THIS_DIR, type=str, help='where to save the trained model')
+  parser.set_defaults(feature=True)
+  return parser.parse_args()
 
 def train(args):
   # set random seed
@@ -44,7 +45,7 @@ def train(args):
     print("Training baseline model:" if args.baseline else "Training HNN model:")
 
   output_dim = args.input_dim if args.baseline else 2
-  nn_model = MLP(args.input_dim, args.hidden_dim, output_dim, args.nonlinearity)
+  nn_model = MLPAutoencoder(args.input_dim, args.hidden_dim, args.latent_dim, args.nonlinearity)
   nn_model.to(device)
   model = HNN(args.input_dim, differentiable_model=nn_model,
             field_type=args.field_type, baseline=args.baseline, device=device)
@@ -86,6 +87,7 @@ def train(args):
     # run test data
     test_ixs = torch.randperm(test_x.shape[0])[:args.batch_size]
     test_dxdt_hat = model.time_derivative(test_x[test_ixs])
+
     #test_dxdt_hat += args.input_noise * torch.randn(*test_x[test_ixs].shape) # add noise, maybe
     test_loss = L2_loss(test_dxdt[test_ixs], test_dxdt_hat)
 
@@ -95,23 +97,23 @@ def train(args):
     if args.verbose and step % args.print_every == 0:
       print("step {}, train_loss {:.4e}, test_loss {:.4e}, grad norm {:.4e}, grad std {:.4e}"
           .format(step, loss.item(), test_loss.item(), grad@grad, grad.std()))
-  '''
-  train_dxdt_hat = model.time_derivative(x)
-  train_dist = (dxdt - train_dxdt_hat)**2
-  test_dxdt_hat = model.time_derivative(test_x)
-  test_dist = (test_dxdt - test_dxdt_hat)**2
-  print('Final train loss {:.4e} +/- {:.4e}\nFinal test loss {:.4e} +/- {:.4e}'
-    .format(train_dist.mean().item(), train_dist.std().item()/np.sqrt(train_dist.shape[0]),
-            test_dist.mean().item(), test_dist.std().item()/np.sqrt(test_dist.shape[0])))
-  '''
-  return model, stats
+  ixs = torch.randperm(x.shape[0])[:1000]
+  x = torch.tensor(x[ixs], requires_grad=True, dtype=torch.float32)
+  x.to(device)
+  enc = model.encoding(x)
+  img = ax.scatter(enc[, enc, enc, c=enc, cmap=plt.hot())
+  fig.colorbar(img)
+  plt.show()
+  return model,  stats
+
 
 if __name__ == "__main__":
-    args = get_args()
-    model, stats = train(args)
+  args = get_args()
+  model, stats = train(args)
 
-    # save
-    os.makedirs(args.save_dir) if not os.path.exists(args.save_dir) else None
-    label = 'baseline' if args.baseline else 'hnn'
-    path = '{}/{}-orbits-{}.tar'.format(args.save_dir, args.name, label)
-    torch.save(model.state_dict(), path)
+
+  # save
+  os.makedirs(args.save_dir) if not os.path.exists(args.save_dir) else None
+  label = 'baseline' if args.baseline else 'hnn'
+  path = '{}/{}-orbits-{}.tar'.format(args.save_dir, args.name, label)
+  torch.save(model.state_dict(), path)
